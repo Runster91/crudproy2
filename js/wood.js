f44d5db54b2120a6103ba2fb9b11a445923a465a
Runster91 <Guillermo.Casillas.l@outlook.com>
2023-07-24 06:05:40 -0400

    Grand

--- js/wood.js
+++ js/wood.js
@@ -3,18 +3,18 @@ window.onload = function () {
 };
 
 function displayData() {
-    var data = JSON.parse(localStorage.getItem("crudData")) || [];
-    var displayDiv = document.getElementById("display-data");
+    const data = JSON.parse(localStorage.getItem("crudData")) || [];
+    const displayDiv = document.getElementById("display-data");
 
     displayDiv.innerHTML = "";
 
     if (data.length === 0) {
         displayDiv.innerHTML = "<p>No data found.</p>";
     } else {
-        var table = "<table><tr><th>Name</th><th>Email</th><th>Actions</th></tr>";
+        let table = "<table><tr><th>Name</th><th>Email</th><th>Actions</th></tr>";
 
-        for (var i = 0; i < data.length; i++) {
-            table += "<tr><td>" + data[i].name + "</td><td>" + data[i].email + "</td><td><button onclick='editData(" + i + ")'>Edit</button><button onclick='deleteData(" + i + ")'>Delete</button></td></tr>";
+        for (let i = 0; i < data.length; i++) {
+            table += `<tr><td>${data[i].name}</td><td>${data[i].email}</td><td><button onclick='editData(${i})'>Edit</button><button onclick='deleteData(${i})'>Delete</button></td></tr>`;
         }
 
         table += "</table>";
@@ -24,12 +24,12 @@ function displayData() {
 
 document.getElementById("crud-form").onsubmit = function (event) {
     event.preventDefault();
-    var name = document.getElementById("name").value;
-    var email = document.getElementById("email").value;
+    const name = document.getElementById("name").value;
+    const email = document.getElementById("email").value;
 
     if (name && email) {
-        var data = JSON.parse(localStorage.getItem("crudData")) || [];
-        data.push({ name: name, email: email });
+        let data = JSON.parse(localStorage.getItem("crudData")) || [];
+        data.push({ name, email });
         localStorage.setItem("crudData", JSON.stringify(data));
 
         document.getElementById("name").value = "";
@@ -39,9 +39,9 @@ document.getElementById("crud-form").onsubmit = function (event) {
 };
 
 function editData(index) {
-    var data = JSON.parse(localStorage.getItem("crudData")) || [];
-    var newName = prompt("Enter updated name:", data[index].name);
-    var newEmail = prompt("Enter updated email:", data[index].email);
+    const data = JSON.parse(localStorage.getItem("crudData")) || [];
+    const newName = prompt("Enter updated name:", data[index].name);
+    const newEmail = prompt("Enter updated email:", data[index].email);
 
     if (newName && newEmail) {
         data[index].name = newName;
@@ -52,7 +52,7 @@ function editData(index) {
 }
 
 function deleteData(index) {
-    var data = JSON.parse(localStorage.getItem("crudData")) || [];
+    const data = JSON.parse(localStorage.getItem("crudData")) || [];
     data.splice(index, 1);
     localStorage.setItem("crudData", JSON.stringify(data));
     displayData();
